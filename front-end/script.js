13ff70c7ed214dc4a8b90776b928b74446603237
Alexander Tekle <tekle.alexander@gmail.com>
2020-05-12 13:42:37 -0500

    Add files via upload

--- front-end/script.js
+++ front-end/script.js
@@ -17,10 +17,10 @@ then(() => {
   bot({
     delay: 700,
     loading: true,
-    content: 'How can I help you today?' }).
-
-  then(() => {
-    chat()
+    content: 'How can I help you today?' }).then(() => {
+      $.get('http://127.0.0.1:5000/topic/load').then(() => {
+        chat()
+    })
   })
 });
 
@@ -32,33 +32,158 @@ function chat() {
         icon: 'user-circle-o',
         sub_type: 'text',
         placeholder: 'What are the symptoms of COVID-19?' }
-  }).then(() => {
-    $.get('http://127.0.0.1:5000/topic/test', function(response){
+  }).then(res => {
+    $.get('http://127.0.0.1:5000/topic/'+res.value, function(response){
       return botui.message.bot({ // second one
         delay: 500,
         loading: true,
-        content: "You're asking a question related to " + response + "; is that correct?" });
-      });
+        content: response });
+      }).then(() => {
+        return botui.message.bot({ // second one
+          delay: 500,
+          type: 'html',
+          loading: true,
+          content: "Did I answer your question?" })
+          .then(() => {
+            return botui.action.button({ // let user choose something
+              delay: 300,
+              action: [
+              {
+                text: 'Yes',
+                value: 'yes' },
+              {
+                text: 'No',
+                value: 'no' }]
+            })
+          }).then(function(answered) {
+            if (answered.value == 'yes') {
+              return botui.message.bot({ // second one
+                delay: 500,
+                type: 'html',
+                loading: true,
+                content: "Great! Is there anything else I can help you with?" })
+              .then(function(){
+                chat();
+              })
+            }
+            else {
+              $.get('http://127.0.0.1:5000/topic/topfivequestions/'+res.value, function(response){
+                botui.message.bot({ // second one
+                  delay: 500,
+                  type: 'html',
+                  loading: true,
+                  content: "Please select one of the five questions or none of the above." });
+
+                botui.message.bot({ // second one
+                  delay: 500,
+                  type: 'html',
+                  loading: true,
+                  content: response })
+                  .then(() => {
+                    question1 = response.split('<br>')[0];
+                    question2 = response.split('<br>')[2];
+                    question3 = response.split('<br>')[4];
+                    var question4 = response.split('<br>')[6]
+                    var question5 = response.split('<br>')[8]
+                    return botui.action.button({ // let user choose something
+                      delay: 300,
+                      action: [
+                      {
+                        text: question1,
+                        value: question1 },
+                      {
+                        text: question2,
+                        value: question2 },
+                      {
+                        text: question3,
+                        value: question3 },
+                      {
+                        text: question4,
+                        value: question4 },
+                      {
+                        text: question5,
+                        value: question5 },
+                      {
+                        text: 'None of the above',
+                        value: 'none' }]
+                    }).then(function(selection) {
+                      if (selection.value != "none") {
+                        $.get('http://127.0.0.1:5000/getanswer/'+selection.value, function(answer){
+                          return botui.message.bot({ // second one
+                            delay: 500,
+                            loading: true,
+                            content: answer });
+                          }).then(() => {
+                            return botui.message.bot({ // second one
+                              delay: 500,
+                              type: 'html',
+                              loading: true,
+                              content: "Did I answer your question?" })
+                              .then(() => {
+                                return botui.action.button({ // let user choose something
+                                  delay: 300,
+                                  action: [
+                                  {
+                                    text: 'Yes',
+                                    value: 'yes' },
+                                  {
+                                    text: 'No',
+                                    value: 'no' }]
+                                })
+                              })
+                              .then(function(answer){
+                                if (answer.value == 'yes') {
+                                  return botui.message.bot({ // second one
+                                    delay: 500,
+                                    type: 'html',
+                                    loading: true,
+                                    content: "Great! Is there anything else I can help you with?" })
+                                    .then(function(){
+                                      chat();
+                                    })
+                                }
+                              })
+                            })
+                      }
+                      else if (selection.value == 'none') {
+                        $.get('http://127.0.0.1:5000/onlinesearch/'+res.value, function(option){
+                          botui.message.bot({ // second one
+                            delay: 500,
+                            type: 'html',
+                            loading: true,
+                            content: "Unfortunately, I don't have an answer for that. Check out this website for an answer to your question." });
+
+                          return botui.message.bot({ // second one
+                            delay: 500,
+                            type: 'html',
+                            loading: true,
+                            content: option });
+                          }).then(function() {
+                            botui.message.bot({ // second one
+                              delay: 500,
+                              type: 'html',
+                              loading: true,
+                              content: "Is there anything else I can help you with?" });
+                          }).then(function(){
+                            chat();
+                          })
+                      }
+                    })
+                  })
+                })
+              }
+
+          });
+      })
+
     /*return botui.message.bot({ // second one
       delay: 500,
       loading: true,
       content: "You're asking a question related to COVID-19 symptoms; is that correct?" });*/
-  }).then(() => {
-    return botui.action.button({ // let user choose something
-      delay: 300,
-      action: [
-      {
-        text: 'Yes',
-        value: 'yes' },
-      {
-        text: 'No',
-        value: 'no' }] });
-  }).then(() => {
+  })/*.then(() => {
     return botui.message.bot({ // second one
       delay: 500,
       loading: true,
       content: "The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually." });
-  }).then(() => {
-    chat();
-  })
+  })*/
 }
--- front-end/server.py
+++ front-end/server.py
@@ -1,26 +1,144 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_cors import CORS
+from googlesearch import search
+import numpy as np
 from sklearn.externals import joblib
 from sklearn.decomposition import NMF
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.stem import PorterStemmer
 import nltk
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import pandas as pd
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_text
 
 st = PorterStemmer()
 app = Flask(__name__)
 CORS(app)
 
-model = joblib.load('../SavedTopicModels/nmf500k_18topics')
-'''tfidf = joblib.load('../SavedTopicModels/tfidf')'''
-tfidf_vec = joblib.load('../SavedTopicModels/tfidf_vec2')
+model = joblib.load('../SavedTopicModels/nmf500kstemmed_14topics')
+tfidf_vec = joblib.load('../SavedTopicModels/tfidf_vec2stemmed14topics')
+
+topics = ["Cases", "Spreading/Getting", "Death Toll", "n/a", "Protection/Masks",
+    "n/a", "n/a", "n/a", "Racism/Xenophobia", "Outbreak Origin", "n/a",
+    "Cruise Ships/dog", "n/a", "america/US/president"]
+
+data = pd.read_excel("Chatbot_qa.xlsx", encoding='utf8')
+module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+
+def respond_highest_scoring_answer(question, label):
+
+
+  # will need to get the data as a dataframe beforehand or sometime here
+  filtered_data = data.loc[data[label] == 1, ['Context', 'Answer']].reset_index(drop=True)
+  response_encodings = module.signatures['response_encoder'](
+        input=tf.constant(filtered_data.Answer),
+        context=tf.constant(filtered_data.Context))['outputs']
+
+  question_encodings = module.signatures['question_encoder'](
+    tf.constant(question)
+  )['outputs']
+  qa_inner_prods = np.inner(question_encodings, response_encodings)
+  qa_array = qa_inner_prods[0]
+  return filtered_data.Answer[np.argmax(qa_inner_prods, axis=1)].values[0]
+
+def show_top_5_questions(question, label):
+
+    #data = pd.read_excel("Chatbot_qa.xlsx", encoding='utf8')
+    #module = hub.load('https://tfhub.dev/google/universal-sentence-encoder-multilingual-qa/3')
+
+    # will need to get the data as a dataframe beforehand or sometime here
+    filtered_data = data.loc[data[label] == 1, ['Context', 'Answer']].reset_index(drop=True)
+    response_encodings = module.signatures['response_encoder'](
+          input=tf.constant(filtered_data.Answer),
+          context=tf.constant(filtered_data.Context))['outputs']
+
+    question_encodings = module.signatures['question_encoder'](
+      tf.constant(question)
+    )['outputs']
+    qa_inner_prods = np.inner(question_encodings, response_encodings)
+    qa_array = qa_inner_prods[0]
+
+    N = 5
+
+    top_N_response_indices = np.argpartition(qa_array, -N)[-N:]
+    sorted_top_response_indicies = top_N_response_indices[np.argsort(qa_array[top_N_response_indices])[::-1][:N]]
+    top_possible_questions = pd.Series.to_numpy(filtered_data.Context[sorted_top_response_indicies])
+    top_possible_questions = np.insert(top_possible_questions, N, 'None of the above questions are similar to what I asked')
+
+    frame = {'Which is your question?': top_possible_questions}
+    questions_df = pd.DataFrame(frame, index=np.arange(1, N+2, 1))
+
+    return '<br><br>'.join(questions_df['Which is your question?'].values)
+
+
+@app.route('/topic/load')
+def load():
+    print("test")
+    return Response(status = 200)
 
 @app.route('/topic/<input>')
 def topic(input):
+
     input_stemmed = []
     input_stemmed.append(" ".join([st.stem(i) for i in input.split()]))
     tfidf_input = tfidf_vec.transform(input_stemmed);
     topic_values = model.transform(tfidf_input);
-    print(topic_values);
+    topic_preds = topic_values.tolist()[0]
+    max = 0;
+    for x in range(1, 14):
+        if x == 3 or x == 6 or x == 7 or x == 10 or x == 12:
+            continue;
+        if topic_preds[x] > topic_preds[max]:
+            max = x;
+    if topic_preds[max] == 0:
+        return "We're sorry, we couldn't answer that question"
+        #prompt a google search
+    elif max == 11:
+        return "You're asking a question related to " + "Cruise Ships / dog" + "; is that correct?"
+    else:
+        print(topics[max])
+        return respond_highest_scoring_answer(input, topics[max])
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/topic/topfivequestions/<input>')
+def topfivequestions(input):
+    print("show top 5 questions")
+    input_stemmed = []
+    input_stemmed.append(" ".join([st.stem(i) for i in input.split()]))
+    tfidf_input = tfidf_vec.transform(input_stemmed);
+    topic_values = model.transform(tfidf_input);
+    topic_preds = topic_values.tolist()[0]
+    max=0;
+    for x in range(1, 14):
+        if x == 3 or x == 6 or x == 7 or x == 10 or x == 12:
+            continue;
+        if topic_preds[x] > topic_preds[max]:
+            max = x;
+    ret = show_top_5_questions(input, topics[max])
+    print(ret);
+
+    return str(ret);
+
+@app.route('/onlinesearch/<input>')
+def onlinesearch(input):
+  search_result_list = list(search(input, tld="com", num=10, stop=3, pause=1))
+  ret = "<a href='" + search_result_list[0] + "' target='_blank'>"+search_result_list[0]+"</a>"
+  return ret
+
+@app.route('/getanswer/<question>')
+def getanswer(question):
+    pd.set_option("display.max_rows", None, "display.max_columns", None)
+    print(data.head())
+    #print(data[data['Context']==question])
+    print(question+"?");
+    print(data[data['Context'] == question + "?"])
+    #print(data[data['Context'] == question + "?"].Answer.tolist()[0])
+    #print(data.loc[[question],['Answer']])
+    #print("lastly: " + str(data[data['Context'] == question].index[0]))
+    return data[data['Context'] == question + "?"].Answer.tolist()[0]
+
+
+#if __name__ == "__main__":
+#    app.run()
